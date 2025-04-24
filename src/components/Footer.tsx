945bdf5551937d0bc2bfff7a02cbf2d881649980
Diego Ortiz <diego.ovazquez@hotmail.com>
2025-04-24 09:09:09 -0600

    feat: header and footer created
    - Added the main components of the portfolio
    - Modified with TailwindCSS

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full z-50 shadow-md font-light bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2023 Your Company. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
