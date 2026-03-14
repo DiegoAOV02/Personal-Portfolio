2c83051a813b2eaf6f611f5088c6a172383997ec
Diego Ortiz <diego.ovazquez@hotmail.com>
2026-03-14 09:31:13 -0600

    build: update package.json
    - update package.json with a new script to allow ci works on github
    - run prettier to format the project

--- src/components/AnimatedSection.tsx
+++ src/components/AnimatedSection.tsx
@@ -2,23 +2,23 @@ import { motion } from "framer-motion";
 
 // This interface Props are used to define the properties for the AnimatedSection component
 interface Props {
-    children: React.ReactNode; // The children elements to be rendered within the animated section
-    delay?: number; // Optional delay before the animation starts
+  children: React.ReactNode; // The children elements to be rendered within the animated section
+  delay?: number; // Optional delay before the animation starts
 }
 
 export default function AnimatedSection({ children, delay = 0 }: Props) {
-    return(
-        <motion.section
-            initial={{ opacity: 0, y: 40 }}
-            whileInView={{ opacity: 1, y:1 }}
-            viewport={{ once: true, margin: "-100px" }}
-            transition={{
-                duration: 0.8,
-                ease: [0.25, 0.1, 0.25, 1],
-                delay,
-            }}
-        >
-            {children}
-        </motion.section>
-    )
+  return (
+    <motion.section
+      initial={{ opacity: 0, y: 40 }}
+      whileInView={{ opacity: 1, y: 1 }}
+      viewport={{ once: true, margin: "-100px" }}
+      transition={{
+        duration: 0.8,
+        ease: [0.25, 0.1, 0.25, 1],
+        delay,
+      }}
+    >
+      {children}
+    </motion.section>
+  );
 }
--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -4,24 +4,28 @@ function About() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center px-4 py-16 bg-gray-700">
       {/* <TypingEffect /> */}
-      <h1 className="text-4xl md:text-5xl font-bold">Welcome to my portfolio!</h1>
-      <div className="h-6" /> 
+      <h1 className="text-4xl md:text-5xl font-bold">
+        Welcome to my portfolio!
+      </h1>
+      <div className="h-6" />
       <p className="max-w-4xl text-justify text-gray-100 text-lg md:text-lg leading-relaxed">
-        I’m Diego, an IT Engineer with a strong interest in building scalable and
-        impactful digital solutions. My background spans web development focused 
-        on sustainability, and data analysis, allowing me to approach problems from both
-        technical and analytical perspectives.
-        <br /><br />
+        I’m Diego, an IT Engineer with a strong interest in building scalable
+        and impactful digital solutions. My background spans web development
+        focused on sustainability, and data analysis, allowing me to approach
+        problems from both technical and analytical perspectives.
+        <br />
+        <br />
         I have experience collaborating with multidisciplinary teams to design,
-        develop, and automate solutions that improve workflows and support project
-        managers and stakeholders in achieving their goals. I enjoy contributing in
-        environments where knowledge sharing, adaptability, and continuous
-        improvement are valued.
-        <br /><br />
-        Beyond professional work, I actively build web applications for personal and
-        exploratory projects. I am highly motivated to learn new technologies and
-        frameworks, and I am always open to joining teams where I can grow, contribute,
-        and create meaningful impact together.
+        develop, and automate solutions that improve workflows and support
+        project managers and stakeholders in achieving their goals. I enjoy
+        contributing in environments where knowledge sharing, adaptability, and
+        continuous improvement are valued.
+        <br />
+        <br />
+        Beyond professional work, I actively build web applications for personal
+        and exploratory projects. I am highly motivated to learn new
+        technologies and frameworks, and I am always open to joining teams where
+        I can grow, contribute, and create meaningful impact together.
       </p>
     </div>
   );
